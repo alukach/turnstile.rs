2ce805efef37f890330cabd45535a2ffa965e086
Anthony Lukach <anthonylukach@gmail.com>
2024-11-04 11:19:23 -0800

    Working list query

--- src/backend.rs
+++ src/backend.rs
@@ -15,55 +15,62 @@ struct BucketRecord {
     created_at: String,
 }
 
+#[worker::send] // https://github.com/cloudflare/workers-rs/issues/485#issuecomment-2008599314
+pub async fn list(postgrest: Postgrest) -> S3Result<dto::ListBucketsOutput> {
+    let res = postgrest
+        .from("bucket")
+        .select("slug, created_at")
+        .execute()
+        .await
+        .map_err(|e| {
+            S3Error::with_message(
+                S3ErrorCode::InternalError,
+                format!("Failed to execute PostgREST query: {e:?}"),
+            )
+        })?;
+
+    let db_buckets = res.json::<Vec<BucketRecord>>().await.map_err(|e| {
+        S3Error::with_message(
+            S3ErrorCode::InternalError,
+            format!("Failed to parse PostgREST response: {e:?}"),
+        )
+    })?;
+
+    let buckets = db_buckets
+        .into_iter()
+        .map(|record| {
+            dto::Timestamp::parse(dto::TimestampFormat::DateTime, record.created_at.as_str())
+                .and_then(|ts| {
+                    Ok(dto::Bucket {
+                        name: Some(record.slug),
+                        creation_date: Some(ts),
+                    })
+                })
+        })
+        .collect::<Result<Vec<dto::Bucket>, ParseTimestampError>>()
+        .map_err(|e| {
+            S3Error::with_message(
+                S3ErrorCode::InternalError,
+                format!("Failed to parse timestamp: {e:?}"),
+            )
+        })?;
+
+    Ok(dto::ListBucketsOutput {
+        buckets: Some(buckets),
+        owner: Some(dto::Owner {
+            display_name: Some("dummy".to_string()),
+            id: Some("dummy".to_string()),
+        }),
+    })
+}
+
 #[async_trait::async_trait]
 impl S3 for DummyBackend {
     async fn list_buckets(
         &self,
         _req: S3Request<dto::ListBucketsInput>,
     ) -> S3Result<S3Response<dto::ListBucketsOutput>> {
-        let req = self
-            .pg
-            .from("bucket")
-            .select("slug, created_at")
-            .execute()
-            .await
-            .map_err(|_e| {
-                S3Error::new(S3ErrorCode::InternalError)
-                // .with_message(format!("Failed to execute PostgREST query: {}", e))
-            })?;
-
-        let db_buckets = req
-            .json::<Vec<BucketRecord>>()
-            .await
-            .map_err(|_e| S3Error::new(S3ErrorCode::InternalError))?;
-
-        let buckets = db_buckets
-            .into_iter()
-            .map(|record| {
-                dto::Timestamp::parse(dto::TimestampFormat::DateTime, record.created_at.as_str())
-                    .and_then(|ts| {
-                        Ok(dto::Bucket {
-                            name: Some(record.slug),
-                            creation_date: Some(ts),
-                        })
-                    })
-            })
-            .collect::<Result<Vec<dto::Bucket>, ParseTimestampError>>()
-            .map_err(|_e| S3Error::new(S3ErrorCode::InternalError))?;
-
-        Ok(S3Response::new(dto::ListBucketsOutput {
-            buckets: Some(buckets),
-            owner: Some(dto::Owner {
-                display_name: Some("dummy".to_string()),
-                id: Some("dummy".to_string()),
-            }),
-        }))
-        // Ok(S3Response::new(dto::ListBucketsOutput {
-        //     buckets: None,
-        //     owner: Some(dto::Owner {
-        //         display_name: Some("dummy".to_string()),
-        //         id: Some("dummy".to_string()),
-        //     }),
-        // }))
+        let buckets = list(self.pg.clone()).await?;
+        Ok(S3Response::new(buckets))
     }
 }
